a83999e8e59e23741fa0a35e5603293f2716c5b8
guicarbar <guicarbar@gmail.com>
2024-04-08 15:36:27 -0300

    Foi criado a abertura do game
    junto delas foi usado a biblioteca time e sys com a a criação de duas funçoes uma para escrever lento e outra para escrever todas as frases de uma lista.
    
    obs: no final do projeto criar um arquivo para colocar as funçoes separadas

--- Game/Coliseu_dos_bizarros.py
+++ Game/Coliseu_dos_bizarros.py
@@ -3,5 +3,31 @@
 import sys
 from time import sleep
 
-# Abertura do game
+# Area reservada para as funções
 
+def write(t):
+    for letra in t:
+        sys.stdout.write(letra)
+        sys.stdout.flush()
+        sleep(0.03)
+        
+
+def imprimir(frases):
+    for f in frases:
+        write('\n' + f + '\n')
+
+
+# introdução do game
+Abertura = [
+    'Narrador: você acorda em uma cela suja e enferrujada com o barulho de uma multidão muito entusiasmada ...',
+    'Narrador: não se lembra como foi parar nesse lugar.',
+    'Narrador: então você se levanta e da uma olhada ao redor ...',
+    'Narrador: observa que não tem nada além de uma grade que você pode ver dois soldados gladiando ao fundo e um velho sentado assistindo.',
+    'Narrado: antes que você possa falar qualquer coisa o velho fala ...',
+    'Velho: Você bebeu demais noite passada, posso ver que nem sabe como foi parar onde está, não tenho tempo para explicar você será o próximo a lutar, se prepare ...',
+    'Narrador: o velho aponta pra umas armas em cima do banco.'
+]
+
+imprimir(Abertura)
+
+#
